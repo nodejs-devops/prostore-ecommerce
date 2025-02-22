f0d5c9580f3b32c56f54e01516a62c7f0c06d1c0
Steven Leong <howsenpl@hotmail.com>
2025-02-23 00:25:23 +0800

    Initial commit

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -10,6 +10,7 @@ import {
 import { auth, signIn, signOut } from '@/auth';
 import { isRedirectError } from 'next/dist/client/components/redirect-error';
 import { hash } from '../encrypt';
+import { hashSync } from 'bcrypt-ts-edge';
 import { prisma } from '@/db/prisma';
 import { formatError } from '../utils';
 import { ShippingAddress } from '@/types';
@@ -66,7 +67,7 @@ export async function signUpUser(prevState: unknown, formData: FormData) {
 
     const plainPassword = user.password;
 
-    user.password = await hash(user.password);
+    user.password = await hashSync(user.password, 10);
 
     await prisma.user.create({
       data: {
