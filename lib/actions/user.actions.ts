4cbc6ac66ac0c5320385a70654064baa3a186f93
Steven Leong <howsenpl@hotmail.com>
2025-02-23 00:26:45 +0800

    Initial commit

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -9,7 +9,7 @@ import {
 } from '../validators';
 import { auth, signIn, signOut } from '@/auth';
 import { isRedirectError } from 'next/dist/client/components/redirect-error';
-import { hash } from '../encrypt';
+// import { hash } from '../encrypt';
 import { hashSync } from 'bcrypt-ts-edge';
 import { prisma } from '@/db/prisma';
 import { formatError } from '../utils';
